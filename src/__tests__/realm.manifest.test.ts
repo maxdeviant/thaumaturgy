aa3532c8fcdc61a6a0d1091769830ddab53a6cab
Marshall Bowers <elliott.codes@gmail.com>
2022-03-05 16:14:36 -0500

    Split up tests long method boundaries

--- src/__tests__/realm.manifest.test.ts
+++ src/__tests__/realm.manifest.test.ts
@@ -1,85 +1,8 @@
-import { faker } from '@faker-js/faker';
 import * as t from 'io-ts';
-import { Realm } from './realm';
-import { Ref } from './ref';
+import { Realm } from '../realm';
+import { Ref } from '../ref';
 
 describe('Realm', () => {
-  describe('define', () => {
-    const Movie = t.type({
-      title: t.string,
-      year: t.Int,
-    });
-
-    describe('when `manifest` is invoked', () => {
-      it('calls the manifester', () => {
-        const realm = new Realm();
-
-        const manifester = jest.fn(() =>
-          Movie.encode({ title: 'Pulp Fiction', year: 1994 as t.Int })
-        );
-
-        realm.define(Movie, { manifest: manifester });
-
-        realm.manifest(Movie);
-
-        expect(manifester).toHaveBeenCalledTimes(1);
-      });
-
-      it('passes a Faker instance to the manifester', () => {
-        const realm = new Realm();
-
-        const manifester = jest.fn(() =>
-          Movie.encode({ title: 'Pulp Fiction', year: 1994 as t.Int })
-        );
-
-        realm.define(Movie, { manifest: manifester });
-
-        realm.manifest(Movie);
-
-        expect(manifester).toHaveBeenCalledWith(faker);
-      });
-    });
-
-    describe('when `persist` is invoked', () => {
-      it('calls the persister', async () => {
-        const realm = new Realm();
-
-        const persister = jest.fn(movie => Promise.resolve(movie));
-
-        realm.define(Movie, {
-          manifest: () =>
-            Movie.encode({ title: 'Arrival', year: 2017 as t.Int }),
-          persist: persister,
-        });
-
-        await realm.persist(Movie);
-
-        expect(persister).toHaveBeenCalledTimes(1);
-      });
-
-      it('passes the manifested entity to the persister', async () => {
-        const realm = new Realm();
-
-        const persister = jest.fn(movie => Promise.resolve(movie));
-
-        realm.define(Movie, {
-          manifest: () =>
-            Movie.encode({ title: 'Arrival', year: 2017 as t.Int }),
-          persist: persister,
-        });
-
-        await realm.persist(Movie);
-
-        expect(persister).toHaveBeenCalledWith(
-          Movie.encode({
-            title: 'Arrival',
-            year: 2017 as t.Int,
-          })
-        );
-      });
-    });
-  });
-
   describe('manifest', () => {
     describe('for a `type` codec', () => {
       const realm = new Realm();
