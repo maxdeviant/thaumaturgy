0ba6d2ae4bd8683702e8f9b497c178f5f7c297d8
Marshall Bowers <elliott.codes@gmail.com>
2022-04-19 15:40:44 -0400

    Enable `--isolatedModules` for `ts-jest`

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '<rootDir>/lib/'],
   clearMocks: true,
